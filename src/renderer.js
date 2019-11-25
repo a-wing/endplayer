924d70e260e59ac59a2b14a07468fb75a624b023
a-wing <1@233.email>
2019-11-25 21:14:34 +0800

    :star: Add DevTools redux

--- src/renderer.js
+++ src/renderer.js
@@ -6,6 +6,14 @@ const ReactDOM = require("react-dom");
 const { remote } = require("electron");
 const { ReactMPV } = require("mpv.js");
 
+
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+
+installExtension(REACT_DEVELOPER_TOOLS)
+.then((name) => console.log(`Added Extension:  ${name}`))
+.catch((err) => console.log('An error occurred: ', err));
+
+
 import { Vvideo, DanmakuDOM, Ass, Status } from "./tsc/engines/engine";
 
 import Log from "./tsc/cores/log";
